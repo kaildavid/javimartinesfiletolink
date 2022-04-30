60807490910160b64cadc03a9b8e41c72c616b00
kaildavid <102341313+kaildavid@users.noreply.github.com>
2022-04-30 06:02:28 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
 api_hash = "64007f363501744b0250d91fc749714f"
 api_id = 15372036
-bot_token = "5321912701:AAG6zBN3fuE--8QZZsFA9DEZhxIoaMEoLAM"
-bot_admin_group = -1001637411118
+bot_token = "5317395920:AAFC22fvsvHUbje8rH9EHgQYFYgEbXGziZs"
+bot_admin_group =  -1001607137860
